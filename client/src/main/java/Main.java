a8a4e23e41d3adcb82324f6c028db81fd548a911
Elizabeth Merrill <eamerri2@asu.edu>
2024-03-01 15:50:50 -0700

    smol edits

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,23 +5,4 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
     }
-
-//    public static class Server {
-//
-//        public int run(int desiredPort) {
-//            Spark.port(desiredPort);
-//
-//            Spark.staticFiles.location("web");
-//
-//            // Register your endpoints and handle exceptions here.
-//
-//            Spark.awaitInitialization();
-//            return Spark.port();
-//        }
-//
-//        public void stop() {
-//            Spark.stop();
-//            Spark.awaitStop();
-//        }
-//    }
 }
